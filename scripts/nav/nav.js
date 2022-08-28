90455624484aa9df173ea479e248a761232f970f
hmcarrico <hmc.carrico@gmail.com>
2022-08-27 18:58:50 -0700

    nav fixed

--- scripts/nav/nav.js
+++ scripts/nav/nav.js
@@ -1,21 +1,21 @@
 async function navHome()
 {
-    // await pageRouter("projects", "remove");
-    // await pageRouter("bio", "remove");
+    await pageRouter("projects", "remove");
+    await pageRouter("bio", "remove");
     await pageRouter("home", "display");
 }
 
 async function navBio()
 {
-//     await pageRouter("projects", "remove");
-//     await pageRouter("home", "remove");
+    await pageRouter("projects", "remove");
+    await pageRouter("home", "remove");
     await pageRouter("bio", "display");
 }
 
 async function navProjects()
 {
-    // await pageRouter("bio", "remove");
-    // await pageRouter("home", "remove");
+    await pageRouter("bio", "remove");
+    await pageRouter("home", "remove");
     await pageRouter("projects", "display");
 }
 
